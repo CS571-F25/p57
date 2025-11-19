96c3b09d3695d6f092cd063f74076a4fd8937c9e
AidanHamilton26 <ahamilton2002@gmail.com>
2025-11-19 16:11:49 -0600

    Navbar background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
+
 function Navbar() {
     return (
-        <nav>
+        <nav style={{ background: 'blue', passing: '1rem'}}>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About Me</Link></li>
